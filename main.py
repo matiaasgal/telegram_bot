7557ac85cc53abaa9719f356cf7759b400091f6d
matiaasgal <matias.gal63@gmail.com>
2026-01-29 15:47:25 -0300

    solucion de errores y typo

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
+import os
 import asyncio
 from telegram import Bot
-import os
 from icalevents.icalevents import events
-from datetime import datetime, timedelta
+
+from datetime import datetime, date, timedelta
+import pytz
+
 
 TOKEN = os.getenv("TOKEN_BOT")
 CHAT_ID = os.getenv("CHAT_ID")
 ICAL_URL = os.getenv("ICAL")
 
+if not TOKEN or not CHAT_ID or not ICAL_URL:
+    print("Error: Falta configurar alguna variable de entorno")
+    exit(1)
+
 async def verificar_eventos():
-    hoy = datetime.now()
-    manana = hoy + timedelta(days=1)
+    tz = pytz.timezone('America/Santiago')
+    utc = pytz.utc
+
+    manana = datetime.now(tz) + timedelta(days=1)
+    inicio = manana.replace(hour=00, minute=00, second=00)
+    fin = manana.replace(hour=23, minute=59, second=59)
+
+    inicio_utc = inicio.astimezone(utc)
+    fin_utc = fin.astimezone(utc)
 
     try:
-        lista_eventos = events(ICAL_URL, start=manana, end=manana)
-        
+        lista_eventos = events(ICAL_URL, start=inicio_utc, end=fin_utc)
+  
         if not lista_eventos:
             print("No hay eventos para mañana")
             return
@@ -22,7 +36,7 @@ async def verificar_eventos():
         mensaje = "Mañana tienes el siguiente evento: "
         for evento in lista_eventos:
             hora = evento.start.strftime("%H:%M")
-            mensaje += f"* {evento.summary} a las {hora} \n"
+            mensaje += f"• {evento.summary} a las {hora} hrs\n"
 
         bot = Bot(token=TOKEN)
         await bot.send_message(chat_id=CHAT_ID, text=mensaje, parse_mode="Markdown")
